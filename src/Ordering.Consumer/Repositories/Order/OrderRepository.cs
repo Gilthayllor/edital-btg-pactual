6293c83183e84fea0c2e06923138ac6cc65bdb7a
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2025-03-10 07:55:43 -0300

    Adição de logs

--- src/Ordering.Consumer/Repositories/Order/OrderRepository.cs
+++ src/Ordering.Consumer/Repositories/Order/OrderRepository.cs
@@ -4,16 +4,19 @@ namespace Ordering.Consumer.Repositories.Order;
 
 public class OrderRepository : IOrderRepository
 {
-    private IMongoCollection<Entities.Order> _orders;
+    private readonly IMongoCollection<Entities.Order> _orders;
+    private readonly ILogger<OrderRepository> _logger;
     
-    public OrderRepository(IMongoClient mongoClient)
+    public OrderRepository(IMongoClient mongoClient, ILogger<OrderRepository> logger)
     {
         var database = mongoClient.GetDatabase("OrderDb");
         _orders = database.GetCollection<Entities.Order>("Orders");
+        _logger = logger;
     }
     
     public async Task InsertAsync(Entities.Order order)
     {
+        _logger.LogInformation("Inserting order {order}", order);
         await _orders.InsertOneAsync(order);
     }
 }
